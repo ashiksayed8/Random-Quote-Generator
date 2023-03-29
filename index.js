e8dcae125e2e6edc41f0ef5c5ec4f80963a5cfc1
ashik-islam <ashiksayed8@gmail.com>
2023-03-30 00:28:40 +0600

    html css js used for Quote1

---
+++ index.js
@@ -0,0 +1,24 @@
+const btnEl = document.getElementById("btn");
+const quoteEl = document.getElementById("quote");
+const authorEl = document.getElementById("author");
+
+
+async function getQuote(){
+    try {
+        btnEl.innerText = "Loading------"
+        btnEl.disabled = true;
+        quoteEl.innerText = `Update-----------`;
+        const result = await fetch("https://api.quotable.io/random")
+                    .then(data => data.json());
+        quoteEl.innerText =  result.content;
+        authorEl.innerText = `~${result.author}`;
+        btnEl.disabled = false;
+        btnEl.innerText = `Get a quote`
+    } catch (error) {
+        quoteEl.innerText =  `An error ,try again later`;
+        authorEl.innerText = `An Error`;
+    }
+}
+
+
+btnEl.addEventListener("click", getQuote);
